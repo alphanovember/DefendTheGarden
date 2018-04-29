c5a6ce824e05a6d12370292f75bfcbd46b03eac7
hasanunl <hu1997tr@gmail.com>
2018-04-29 20:02:33 +0300

    Current Game

---
+++ DefendTheGarden/Assets/Scripts/PersistentMusic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistentMusic : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+	private AudioSource audioSource;
+
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void OnLevelWasLoaded (int level)  {
+		AudioClip thisLevelMusic = levelMusicChangeArray [level];
+		if (thisLevelMusic) { // if there is some music attached
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true ;
+			audioSource.Play() ;
+		} 
+	}
+}
